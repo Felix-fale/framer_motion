bbde9c03e70db339e3196e164ddeb4538e531c5a
Felix-fale <falefelix23@gmail.com>
2023-07-29 23:12:51 +0000

    create Awesome text animation with framer-motion

---
+++ FramerAnimation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+function FramerAnimation() {
+  return (
+    <div>
+      <motion.h1
+        initial={{ x: -1000 }}
+        animate={{ x: [0, 900, 0] }}
+        transition={{ duration: 3, delay: 0.2 }}
+        whileHover={{ scale: 0.9, opacity: 0.2 }}
+      >
+        It is H1 for animation using Framer Motion.
+      </motion.h1>
+    </div>
+  );
+}
+
+export default FramerAnimation;
